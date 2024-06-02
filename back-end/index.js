80721c6e51231bdcf7b0fea38bc8b347c9066784
ImanSohail29 <imanmirza2197@gmail.com>
2024-06-02 17:21:27 +0500

    error handler

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const todoRoutes = require('./routes/todoRoutes');
-const errorHandler = require('./middlewares/errorHandler');
+const errorHandler = require("./middlewares/errorhandler");
 const sequelize = require('./config/db');
 const Todo = require('./models/todo');
 
